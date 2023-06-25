6d38c6823867d2de8a94ece5da28976cbb4ee5e2
Daniel Olaya <daniel.olayam@gmail.com>
2023-06-24 20:12:32 -0700

    ch01q7,8

--- Chapter01/ch01q6-rand/src/main.rs
+++ Chapter01/ch01q6-rand/src/main.rs
@@ -1,12 +1,18 @@
 use rand::Rng;
 
 fn main() {
-    println!("Hello, rand!");
     let mut rng = rand::thread_rng();
-    for _ in 0..10 {
-        let random_numbers: f32 = rng.gen_range(100.0..=400.0);
-        println!("{}", random_numbers)
+    //println!("Generating random numbers");
+    //for _ in 0..10 {
+    //    let random_numbers: i32 = rng.gen_range(100..=400);
+    //    println!("{}", random_numbers)
+    //}
+
+    println!("Generating squared ints");
+    let mut squares: Vec<i32> = Vec::new();
+    for i in 1..=200 {
+        let sq : i32 = i * i;
+        squares.push(sq);
+        println!("{}", sq);
     }
-
-
 }
